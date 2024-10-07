abc9f35e83aecc5f3e2ffc3461c7110142b67ef5
Lockl00p <97256723+Lockl00p@users.noreply.github.com>
2024-10-07 06:46:00 +0000

    Starting,,, somewhere

---
+++ os/wasm/pre.js
@@ -0,0 +1,75 @@
+//current command in ascii decimal
+let currentcmd = [0,0,0] 
+let currentfile = "";
+const sleep = ms => new Promise(r => setTimeout(r,ms));
+var loaded = false;
+Module['print'] = function(text){console.log(text);}
+Module['preRun'] = function()
+{
+    
+    function stdin(){return 10};
+    var stdout = null;
+    var stderr = null; 
+    FS.init(stdin,stdout,stderr);
+    FS.mount(IDBFS,{},"/home/web_user/");
+    
+}
+Module['noInitialRun'] = true
+
+function keyev(ev) {
+        home = "/home/web_user/fallout1"
+        if(ev.key == "`"){
+        file_selector.click()
+        }
+        else if(ev.key == "="){
+            FS.syncfs(false,function(){alert("save attempted")});
+        }
+        else if(ev.key == "\\"){
+            readfrom = home.concat('',prompt("Read which directory?"))
+            alert(readfrom)
+            alert(FS.readdir(readfrom))
+            
+            
+        } else if(ev.key == "]"){
+            FS.syncfs(true,function(){
+                try {
+                    FS.mkdir("/home/web_user/fallout1")
+                    FS.mkdir("/home/web_user/fallout1/DATA")
+                } catch (error) {
+                    
+                }
+                loaded = true;
+            });
+        }
+    
+    
+    
+}
+
+document.addEventListener('keydown', keyev, true);
+
+document.addEventListener('click', (ev) => {
+    console.log("event is captured only once.");
+    args = []
+    if(!loaded){
+    FS.syncfs(true,function(){
+        try {
+            FS.mkdir("/home/web_user/fallout1/")
+            FS.mkdir("/home/web_user/fallout1/DATA")
+        } catch (error) {
+            
+        }
+        FS.chdir("/home/web_user/fallout1");
+        document.removeEventListener("keydown",keyev,true);
+        document.getElementById("Instructions").remove();
+        Module.callMain(args);
+});}
+    else{
+        FS.chdir("/home/web_user/fallout1");
+        document.removeEventListener("keydown",keyev,true);
+        document.getElementById("Instructions").remove();
+        Module.callMain(args);
+    }
+    
+  }, { once: true });
+
